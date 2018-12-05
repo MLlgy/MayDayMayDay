429cec2b4135c06fc69bda949c1a6e22d7a5a80f
liguoying <18363851609@163.com>
2018-12-05 18:11:17 +0800

    draw test

--- customclock/src/main/java/com/lgy/customclock/widget/CustomClock.java
+++ customclock/src/main/java/com/lgy/customclock/widget/CustomClock.java
@@ -1,13 +1,17 @@
 package com.lgy.customclock.widget;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
+import android.os.Build;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
+
+import com.lgy.customclock.R;
 
 public class CustomClock extends View {
     private int mWidth;
@@ -16,6 +20,7 @@ public class CustomClock extends View {
     private Paint mArchPaint;
     private int mRadiu;
     private RectF mRectF;
+    private Paint mTextPaint;
 
     public CustomClock(Context context) {
         this(context, null);
@@ -30,6 +35,7 @@ public class CustomClock extends View {
         init();
     }
 
+    @TargetApi(Build.VERSION_CODES.M)
     private void init() {
         this.post(new Runnable() {
             @Override
@@ -42,24 +48,30 @@ public class CustomClock extends View {
                         mRadiu * 2 + mWidth / 14, mHeight / 2 + mRadiu);
             }
         });
-        mArchPaint = new Paint();
         mCirclePaint = new Paint();
-        mCirclePaint.setColor(Color.WHITE);
-        mCirclePaint.setStrokeWidth(2.0f);
-        mCirclePaint.setAntiAlias(true);
-        mCirclePaint.setStyle(Paint.Style.STROKE);
+        mArchPaint = new Paint();
+        mArchPaint.setColor(Color.WHITE);
+        mArchPaint.setStrokeWidth(2.0f);
+        mArchPaint.setAntiAlias(true);
+        mArchPaint.setStyle(Paint.Style.STROKE);
+
+        mTextPaint = new Paint();
+        mTextPaint.setTextSize(50f);
+        mTextPaint.setColor(getResources().getColor(R.color.colorWhite, null));
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.save();
+        canvas.drawLine(mWidth / 2, 0, mWidth / 2, mHeight, mArchPaint);
+        canvas.drawLine(0, mHeight / 2, mWidth, mHeight / 2, mArchPaint);
 //        canvas.translate(mWidth / 2, mHeight / 2);
         for (int i = 0; i < 4; i++) {
-            canvas.drawArc(mRectF, 5, 80, true, mCirclePaint);
+            canvas.drawArc(mRectF, 5, 80, false, mArchPaint);
             canvas.rotate(90f, mWidth / 2, mHeight / 2);
             String mNum = String.valueOf(3 * (i + 1));
-//            canvas.drawText(mNum,);
+            canvas.drawText(mNum, mWidth / 2 - mTextPaint.measureText(mNum) / 2, mHeight / 2 - mRadiu + 20, mTextPaint);
         }
 
     }
